700677295d8f30c1453abeb305af608baf22dd73
Tobi <tyler.lopez15@icloud.com>
2024-08-06 18:32:43 -0500

    made a change

--- POP/notifications/models.py
+++ POP/notifications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TwilioSMS(models.Model):
+    # TwilioSMS model will store all SMS
+    pass
+
+class DjangoSMTP(models.Model):
+    # DjangoSMTP model will store all emails
+    pass
--- POP/outages/views.py
+++ POP/outages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import OutageReport
 from .forms import OutageReportForm
+from notifications.models import TwilioSMS, DjangoSMTP
 
 
 # Create your views here.
@@ -17,3 +18,15 @@ def report_outage(request):
     else:
         form = OutageReportForm()
     return render(request, 'outages/report_outage.html', {'form': form})
+
+@login_required
+def update_outage_report(request):
+    # Don't forget to incorporate trigger function for notifications
+    pass
+
+def get_user_outage_report(request, **extra_kwargs):
+    user_reports = OutageReport.objects.filter(user_id=request.user)
+    return 
+
+def get_all_outage_reports():
+    pass
