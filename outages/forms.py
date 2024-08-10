d03a1c0c7c5949aa6729ea8bdb3748c0773a6b91
Placido Garay <placidogaray2001@gmail.com>
2024-08-10 01:25:21 -0500

    Update forms.py

--- outages/forms.py
+++ outages/forms.py
@@ -5,4 +5,10 @@ class OutageReportForm(forms.ModelForm):
     class Meta: 
         model = OutageReport
         fields = ['location', 'description', 'status', 'planned', 'estimated_restoration_time']
+
+
+class OutageUpdateForm(forms.ModelForm):
+    class Meta:
+        model = OutageReport
+        fields = ['status', 'estimated_resoration_time']
         
