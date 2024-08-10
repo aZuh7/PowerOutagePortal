2a1e4ecd755e505e90914323852861a52be4ae91
Placido Garay <placidogaray2001@gmail.com>
2024-08-10 01:24:40 -0500

    Update urls.py

--- outages/urls.py
+++ outages/urls.py
@@ -3,4 +3,7 @@ from .views import report_outage, update_outage_report, get_user_outage_report, 
 
 urlpatterns = [
     path('report/', report_outage, name='report_outage'),
+    path('UpdatedReport/', update_outage_report, name='update_outage_report')
+    path('user_report/', get_user_outage_report, name='get_user_outage_report')
+    path('all_reports/',get_all_outage_reports, name='get_all_outage_reports' )
 ]
