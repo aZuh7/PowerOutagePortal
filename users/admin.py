3dda9de51e9de18cb7774f00045d63bd931e4a24
Tobi Azuh <Tyler.lopez15@icloud.com>
2024-08-09 23:17:26 -0500

    Users admin.py configuration
    Configured users.admin.py to allow viewing user groups and outage report groups

--- users/admin.py
+++ users/admin.py
@@ -2,16 +2,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from outages.models import OutageReport
 
+class OutageReportInline(admin.TabularInline):
+    model = OutageReport
+    extra = 0
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    inlines = (OutageReportInline,)
+    list_display = ('user_id', 'first_name', 'last_name', 'username', 'email', 'is_staff', 'is_superuser')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('username', 'first_name', 'last_name', 'phone', 'address', 'zip')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
@@ -20,6 +24,16 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
     search_fields = ('email', 'username')
-    ordering = ('email',)
+    ordering = ('user_id',)
 
 admin.site.register(User, CustomUserAdmin)
+
+class CustomOutageAdmin(admin.ModelAdmin):
+    model = OutageReport
+    list_display = ('report_id', 'user_id', 'report_date', 'location', 'description', 'planned', 'estimated_restoration_time')
+    list_filter = ('report_id', 'user_id', 'report_date', 'planned')
+
+    search_fields = ('report_id', 'user_id', 'report_date')
+    ordering = ('report_date',)
+
+admin.site.register(OutageReport, CustomOutageAdmin)
