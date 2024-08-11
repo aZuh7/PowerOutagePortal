01f5430583f4b32032b952d6ebba1d97ade0bc0c
dm9920 <martinezd260@gator.uhd.edu>
2024-08-10 19:51:31 -0500

    WIP_Branch
    Worked on get_user_outage_report(), get_all_outage_reports(), update_outage_reports()

--- outages/forms.py
+++ outages/forms.py
@@ -14,4 +14,13 @@ class OutageReportForm(forms.ModelForm):
         if is_admin and not is_update:
             self.fields['status'] = forms.ChoiceField(choices=OutageReport.STATUS_CHOICES, disabled=not is_admin)
 
-        
+class FilterOutageReport(forms.ModelForm):
+    report_id = forms.IntegerField(required=False, label='Report ID')
+    status = forms.ChoiceField(choices=[('', 'All')] + list(OutageReport.STATUS_CHOICES), required=False, label='Status')
+    planned = forms.ChoiceField(choices=[('', 'All'), ('True', 'Yes'), ('False', 'No')], required=False, label='Planned')
+
+    def __init__(self, *args, **kwargs):
+        super(FilterOutageReport, self).__init__(*args, **kwargs)
+        self.fields['status'].label = 'Filter by Status'
+        self.fields['planned'].label = 'Filter by Planned'
+        self.fields['report_id'].label = 'Filter by Report ID'
--- outages/views.py
+++ outages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import OutageReport
-from .forms import OutageReportForm
+from .forms import OutageReportForm, FilterOutageReport
 
 '''
 The following function is a view function that loads the outage report form. 
@@ -27,9 +27,21 @@ def report_outage(request):
 The following function is a view function that updates an outage report.
 '''
 @login_required
-def update_outage_report(request):
+def update_outage_report(request, report_id):
     # Don't forget to incorporate trigger function for notifications
-    pass
+    try:
+        report = OutageReport.objects.get(pk = report_id)
+    except OutageReport.DoesNotExist:
+        pass
+    if request.method == 'POST':
+        form = OutageReportForm(request.POST, instance = report)
+        if form.is_valid():
+            form.save()
+            # notify_users(report)
+    else:
+        form = OutageReport(instance = report)
+    return render(request, 'outages/update_outage_report.html', {'form': form})
+
 
 '''
 The following function is a view function that deletes an outage report.
@@ -38,12 +50,26 @@ The following function is a view function that deletes an outage report.
 @login_required
 def get_user_outage_report(request, **extra_kwargs):
     user_reports = OutageReport.objects.filter(user_id=request.user)
-    return 
+    return render(request, 'outages/user_outage_reports.html', {'user_reports': user_reports})
 
 '''
 The following function is a view function that retrieves all outage reports.
 '''
 
 @login_required
-def get_all_outage_reports():
-    pass
+def get_all_outage_reports(request):
+    if request.user.user.type == 'admin':
+        form = FilterOutageReport(request.GET or None)
+        reports = OutageReport.objects.all()
+
+        if form.is_valid():
+            report_id = form.cleaned_data.get('report_id')
+            if report_id:
+                reports = reports.filter(report_id=report_id)
+            status = form.cleaned_data.get('status')
+            if status:
+                reports = reports.filter(status=status)
+            planned = form.cleaned_data.get('planned')
+            if planned:
+                reports = reports.filter(planned=(planned == 'True'))
+    return render(request, 'outages/admin_dashboard.html', {'reports': reports, 'form': form})
